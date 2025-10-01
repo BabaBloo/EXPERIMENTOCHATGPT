61f48afa8a28acc357b5dce47cb2179ba23aba06
BabaBloo <luzflorescontactos@gmail.com>
2025-09-30 22:54:31 -0300

    2

---
+++ frases.js
@@ -0,0 +1,14 @@
+// Poné acá todas las variaciones que quieras.
+// Podés ampliarlo cuando quieras sin tocar index.html.
+window.frases = [
+  "Hola, me interesa abrir mi cuenta",
+  "Buenas, quiero registrarme en la plataforma",
+  "Me gustaría crear un usuario nuevo",
+  "Quiero generar un acceso, mi nombre es",
+  "Hola 👋 ¿me ayudan a darme de alta?",
+  "Necesito abrir perfil, soy",
+  "Hola, deseo iniciar por primera vez",
+  "Buenas, me gustaría sumarme",
+  "Quiero empezar a usar la cuenta",
+  "Me gustaría registrarme ahora mismo"
+];
